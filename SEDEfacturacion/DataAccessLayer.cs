766fd12235797025e057272861f84a573b83643d
mserrano33 <serrano.matias.sebastian@gmail.com>
2022-08-05 16:21:23 -0300

    comman parameters

--- SEDEfacturacion/DataAccessLayer.cs
+++ SEDEfacturacion/DataAccessLayer.cs
@@ -18,11 +18,26 @@ namespace SEDEfacturacion
             {
                 conn.Open();
                 string query =@"
-                                 INSERT INTO Clientes(Nombre, DNI, Direccion,
-                                 Telefono, Id_estado)";
+                                 INSERT INTO Clientes(Nombre, DNI, Direccion, Telefono, Id_estado)
+                                 VALUES (@Nombre, @DNI, @Direccion, @Telefono, Id_estado)";
+                SqlParameter Nombre = new SqlParameter();
+                Nombre.ParameterName = "@Nombre";
+                Nombre.Value = cliente.Nombre;
+                Nombre.DbType = System.Data.DbType.String;
+
+                SqlParameter DNI = new SqlParameter("@DNI", cliente.DNI);
+                SqlParameter Direccion = new SqlParameter("@Direccion", cliente.Direccion);
+                SqlParameter Telefono = new SqlParameter("@Telefono", cliente.Telefono);
+                SqlParameter Id_estado = new SqlParameter("@Id_estado", cliente.Id_estado);
+
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.Add(Nombre);
+                command.Parameters.Add(DNI);
+                command.Parameters.Add(Direccion);
+                command.Parameters.Add(Telefono);
+                command.Parameters.Add(Id_estado);
 
 
-                SqlCommand comand = new SqlCommand(query, conn);
             }
             catch (Exception)
             {
